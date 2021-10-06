b00af34de16970284f89d944c4659a0222480780
Oktay Esgul <34231271+oesgul@users.noreply.github.com>
2021-10-06 08:41:58 +0300

    Update app.js wed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello wonderful world6!\n');
+  res.send('Hello wonderful world7!\n');
 });
 
 app.listen(8080, function () {
